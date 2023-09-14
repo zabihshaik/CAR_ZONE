f61bf5e09b078ef5fbf0cdbd02715ce73e10651b
Zabihulla Shaik <informshaikz@gmail.com>
2023-09-14 23:58:11 +0530

    Layout Done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,7 @@ const Navbar = () => {
         <Link href='/' className='flex justify-center items-center'>
           <Image src='/logo.svg' alt='Car Zone Logo' width={118} height={18} className='object-contain'/>
         </Link>
+        <CustomButton title='Sign In' btnType='button' containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'/>
       </nav>
     </header>
   )
