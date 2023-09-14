65892dce829a34cbab0354b9da6bf85996c46ac6
Zabihulla Shaik <informshaikz@gmail.com>
2023-09-15 00:48:51 +0530

    Fix footer alignment for small devices

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,35 +13,37 @@ const Footer = () => {
             All rights reserved &copy;
           </p>
         </div>
+
         <div className='footer__links'>
           {footerLinks.map((link) => (
             <div key={link.title} className='footer__link'>
               <h3 className='font-bold'>{link.title}</h3>
-              {link.links.map((item) => (
+              <div className="flex flex-col gap-5">
+                {link.links.map((item) => (
                 <Link key={item.title} href={item.url} className='text-gray-500'>
                   {item.title}
                 </Link>
               ))}
+              </div>
             </div>
           ))}
         </div>
+      </div>
+
+      <div className='flex flex-wrap gap-4 justify-between items-center mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+        <p>@2023 CarHub. All Rights Reserved</p>
+
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms & Conditions
+          </Link>             
         </div>
-
-        <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
-          <p>@2023 CarHub. All Rights Reserved</p>
-
-          <div className="footer__copyrights-link">
-            <Link href="/" className="text-gray-500">
-              Privacy & Policy
-            </Link>
-            <Link href="/" className="text-gray-500">
-              Terms & Condition
-             </Link>             
-          </div>
-        
       </div>
     </footer>
   )
 }
 
-export default Footer
+export default Footer;
