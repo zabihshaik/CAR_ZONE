e531e1ef9bd55b43b3ce18d4696b36389bddce65
Zabihulla Shaik <informshaikz@gmail.com>
2023-09-16 23:26:40 +0530

    Car Catalogue Start

---
+++ components/SearchBar.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const SearchBar = () => {
+  const handleSearch = () => {} 
+  
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+
+    </form>
+  )
+}
+
+export default SearchBar
