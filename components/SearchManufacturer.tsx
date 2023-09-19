fc88e8bd71907ec307d4f39a46ed991a58ca68aa
Zabihulla Shaik <informshaikz@gmail.com>
2023-09-19 23:05:46 +0530

    Remove create query from filtered manufacturers

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Fragment, useState } from 'react';
+import { useState, Fragment } from 'react';
 import Image from 'next/image';
 import { Combobox, Transition } from '@headlessui/react';
 import { manufacturers } from '@/constants';
@@ -31,15 +31,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
             leaveFrom='opacity-100' leaveTo='opacity-0' 
             afterLeave={() => setQuery('')}>
               <Combobox.Options>
-              {filteredManufacturers.length === 0 && query !== "" ? (
-                <Combobox.Option
-                  value={query}
-                  className='search-manufacturer__option'
-                >
-                  Create "{query}"
-                </Combobox.Option>
-              ) : (
-                filteredManufacturers.map((item) => (
+              {filteredManufacturers.map((item) => (
                   <Combobox.Option
                     key={item}
                     className={({ active }) =>
@@ -63,7 +55,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                     )}
                   </Combobox.Option>
                 ))
-              )}
+              }
               </Combobox.Options>
           </Transition>
         </div>
